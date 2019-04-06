4ff799ae27e433b77ef228c4b063e446019e81de
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-04-06 21:11:38 +0200

    Initial Commit

---
+++ POI.php
@@ -0,0 +1,37 @@
+<?php
+
+class POI
+{
+	private static $instance;
+	public $db;
+	public $singular;
+	public $path;
+	public $plural;
+	public $customFields;
+	public $nameSpace;
+	public $icon;
+
+	public function __construct($singular, $plural, $path, $customFields)
+	{
+		$this->singular = $singular;
+		$this->plural = $plural;
+		$this->db = 'bs-poi';
+		$this->nameSpace = 'bs_poi';
+		$this->path = $path;
+		$this->icon = 'dashicons-hammer';
+		$this->customFields = $customFields;
+	}
+
+	public static function getInstance($singular, $plural, $path, $customFields)
+	{
+		if (!self::$instance instanceof self) {
+			self::$instance = new self($singular, $plural, $path, $customFields);
+		}
+		return self::$instance;
+	}
+
+}
+
+
+
+
